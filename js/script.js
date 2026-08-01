48c581a42fcdba8a274f65cdacadf4e0a19cb344
Dell <dellanourian@gmail.com>
2026-08-01 18:10:41 +0330

    add js

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const menuBtn = document.querySelector(".menu-btn");
+const navbar = document.querySelector(".navbar");
+
+menuBtn.addEventListener("click", () => {
+
+    navbar.classList.toggle("active");
+
+});
